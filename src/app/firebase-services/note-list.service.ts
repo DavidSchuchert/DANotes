c9a570f6fd680b70d8e6c505ec5cde8e16eb9610
DavidSchuchert <Davidschuchert@icloud.com>
2023-12-10 11:50:48 +0100

    added updateDoc

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -5,6 +5,8 @@ import {
   doc,
   collectionData,
   onSnapshot,
+  addDoc,
+  updateDoc,
 } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Note } from '../interfaces/note.interface';
@@ -13,8 +15,8 @@ import { Note } from '../interfaces/note.interface';
   providedIn: 'root',
 })
 export class NoteListService {
-trashNotes: Note[] = [];
-normalNotes: Note[] = [];
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
 
   unsubTrash;
   unsubNotes;
@@ -24,6 +26,44 @@ normalNotes: Note[] = [];
   constructor() {
     this.unsubTrash = this.subTrashList();
     this.unsubNotes = this.subNotesList();
+  }
+
+  async updateNote(note: Note) {
+    if (note.id) {
+      let docRef = this.getSingleDocRef(this.getColIdFromNote(note), note.id);
+      await updateDoc(docRef, this.getCleanJson(note))
+        .catch((err) => {
+          console.log(err);
+        })
+        .then();
+    }
+  }
+
+  getCleanJson(note:Note):{} {
+    return {
+      type: note.type,
+      title: note.title,
+      content: note.content,
+      marked: note.marked,
+    }
+  }
+
+  getColIdFromNote(note: Note) {
+    if (note.type == 'note') {
+      return 'notes';
+    } else {
+      return 'trash';
+    }
+  }
+
+  async addNote(item: Note) {
+    await addDoc(this.getNotesRef(), item)
+      .catch((err) => {
+        console.error(err);
+      })
+      .then((docRef) => {
+        console.log('Document written with ID: ', docRef?.id);
+      });
   }
 
   ngonDestroy() {
@@ -44,7 +84,7 @@ normalNotes: Note[] = [];
     });
   }
 
-  subNotesList(){
+  subNotesList() {
     return onSnapshot(this.getNotesRef(), (list) => {
       this.normalNotes = [];
       list.forEach((element) => {
